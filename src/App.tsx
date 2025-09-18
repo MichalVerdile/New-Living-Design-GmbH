7a212998077bb99a3eaa1f7604147cf1e1b20500
verdiant <verdiant@student.zhaw.ch>
2025-09-18 14:43:13 +0200

    Fix import statement for SpeedInsights component to use the correct package

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 import ScrollToTop from './components/scroll-helper/ScrollToTop';
 import Partners from './pages/partners/Partners';
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 function App() {
